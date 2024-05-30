c1d2e8a0698285c60b5751bc5111860e3de6f48a
haithembourbia <haithem.bourbia@dipower.fr>
2024-05-30 23:58:32 +0100

    add auth & user module

--- main.js
+++ main.js
@@ -1,41 +1,65 @@
-//imports
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const session = require('express-session');
+require("dotenv").config()
+require("express-async-errors")
 
-const app = express();
-const PORT = process.env.PORT || 4000;
+const express = require("express")
+const app = express()
 
-//database connesction 
-mongoose.connect(process.env.DB_URI, {useUnifiedTopology:true});
-const db = mongoose.connection;
-db.on("error", (error)=> console.log(error));
-db.once("open", () => console.log("Connected to the database!"));
+// Rest of the packages
+const morgan = require("morgan") //HTTP request logger middleware
+const cookieParser = require("cookie-parser")
 
-//middlewares
-app.use(express.urlencoded({extended:false}));
-app.use(express.json());
+// Require Database
+const connectDB = require("./db/connect")
 
-app.use(session({
-    secret:'my secret key',
-    saveUninitialized:true,
-    resave:false,
-}));
+// Require Routes
+const authRouter = require("./routes/authRoutes")
+const userRouter = require("./routes/userRoutes")
 
-app.use((req, res, next)=>{
-    res.locals.message = req.session.message;
-    delete req.session.message;
-    next();
-});
 
-//set template engine
-app.set("view engine", "ejs");
+// Require Middleware
+const notFoundMiddleware = require("./middleware/not-found")
+const errorHandlerMiddleware = require("./middleware/error-handler")
 
-//route prefix
-app.use("",require('./routes/routes'))
+// Invoke Extra packages
+app.use(morgan("tiny"))
+app.use(express.json())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static("./public"))
 
-app.listen(PORT,()=>{
-    console.log(`Server started at http://localhost:${PORT}`);
-});
+// Home get
+app.get("/", (req, res) => {
+  res.send("<h1> E-Commerce API</h1>")
+})
 
+
+// Testing route
+app.get("/api/v1/", (req, res) => {
+  // console.log(req.cookies)
+  // console.log(req.signedCookies)
+  res.send("E-commerce API")
+})
+
+
+// Invoke Routers
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/auth", authRouter)
+
+
+// Invoke Middleware
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const port = process.env.PORT || 5000
+const start = async () => {
+  try {
+    // Connect database
+    await connectDB(process.env.MONGO_URL)
+    app.listen(port, () =>
+      console.log(`🚀 Server is listening on port ${port}...`)
+    )
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
